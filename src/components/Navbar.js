38361f8aa4d1b5001c0471a31f7e0e605b4e5780
yuriy17 <yuriy_sh@zoho.com>
2020-08-06 20:48:05 +0300

    style:  burger animation, fix overflow, add shadow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 export default class Navbar {
   constructor() {
     this.menuListElement = document.querySelector('.menu__list');
+    this.burger = document.querySelector('#burger');
   }
   
   init(){
@@ -9,6 +10,8 @@ export default class Navbar {
         event.target.classList.toggle('arrow_active');
       }
     });
-
+    this.burger.addEventListener('click',()=>{
+      document.body.classList.toggle('lock');
+    });
   }
 };
